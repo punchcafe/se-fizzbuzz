f2d2f1c7229ec6c659d2fb019973d9f79fa602de
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-04-17 00:02:26 +0100

    :whale: simple docker configuration to launch api

--- api/config/dev.exs
+++ api/config/dev.exs
@@ -2,10 +2,10 @@ use Mix.Config
 
 # Configure your database
 config :api, Api.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "api_dev",
-  hostname: "localhost",
+  username: System.get_env("DB_USERNAME", "postgres"),
+  password: System.get_env("DB_PASSWORD", "postgres"),
+  database: System.get_env("DB_NAME", "api_dev"),
+  hostname: System.get_env("DB_HOST", "localhost"),
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
