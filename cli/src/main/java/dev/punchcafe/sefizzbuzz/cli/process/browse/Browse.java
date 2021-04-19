fc4ac5f72653486c99de7bc26b43a24b878fe052
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-04-19 22:02:04 +0100

    cli: :sparkles: add page size and page jump functionality to BROWSE

--- cli/src/main/java/dev/punchcafe/sefizzbuzz/cli/process/browse/Browse.java
+++ cli/src/main/java/dev/punchcafe/sefizzbuzz/cli/process/browse/Browse.java
@@ -14,12 +14,13 @@ import static dev.punchcafe.sefizzbuzz.cli.process.browse.PageRenderer.renderPag
 @Builder
 public class Browse implements AppProcess {
 
-    //private static Pattern CHANGE_PAGE_SIZE = Pattern.compile("psize (\\d)");
-    //private static Pattern CHANGE_PAGE = Pattern.compile("pjump (\\d)");
+    private static Pattern CHANGE_PAGE_SIZE = Pattern.compile("psize (\\d+)");
+    private static Pattern CHANGE_PAGE = Pattern.compile("pjump (\\d+)");
     private static String NEXT_PAGE = "n";
     private static String PREVIOUS_PAGE = "p";
     private static String EXIT = "exit";
-
+    private static String HELP = "help";
+    private static String HELP_MESSAGE = "available commands are: p, n, psize NUM, pjump NUM, help, exit";
     // default to page one
     private int page;
     private int pageSize;
@@ -37,16 +38,24 @@ public class Browse implements AppProcess {
         final var firstPage = fizzBuzzClient.getPage(pageSize, page);
         userOutputWriter.printToUser(renderPage(firstPage.getData(), firstPage.getPage()));
         var command = userInputReader.getUserInput().trim();
-        System.out.println(command);
-        System.out.println(page);
         while(!EXIT.equals(command)){
-            if(NEXT_PAGE.equals(command)){
+            final var changePageNumberMatcher = CHANGE_PAGE.matcher(command);
+            final var changePageSizeMatcher = CHANGE_PAGE_SIZE.matcher(command);
+            if(HELP.equals(command)){
+                userOutputWriter.printToUser(HELP_MESSAGE);
+                command = userInputReader.getUserInput();
+                continue;
+            } else if(NEXT_PAGE.equals(command)){
                 page++;
-                System.out.println(page);
             } else if(PREVIOUS_PAGE.equals(command)){
                 if(page > 1){
                     page--;
                 }
+            } else if(changePageNumberMatcher.matches()){
+                page = Integer.parseInt(changePageNumberMatcher.group(1));
+            } else if(changePageSizeMatcher.matches()){
+                System.out.println("you got it!");
+                pageSize = Integer.parseInt(changePageSizeMatcher.group(1));
             }
             final var nextPage = fizzBuzzClient.getPage(pageSize, page);
             userOutputWriter.printToUser(renderPage(nextPage.getData(), nextPage.getPage()));
